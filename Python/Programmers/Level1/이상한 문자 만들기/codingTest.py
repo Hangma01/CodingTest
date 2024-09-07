647cdbbb492b3826c1cc17945c649c2fe3590923
Hangma01 <dn09yooon@gmail.com>
2024-09-07 10:47:23 +0900

    이상한 문자 만들기

---
+++ Python/Programmers/Level1/이상한 문자 만들기/codingTest.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = []
+    s = s.split(' ')
+    for word in s:
+        temp = ''
+        for i in range(len(word)):
+            if i % 2 == 0:
+                temp += word[i].upper()
+            else:
+                temp += word[i].lower()
+        answer.append(temp)
+    return ' '.join(answer)
+
+s="try hello world"
+print(solution(s))
