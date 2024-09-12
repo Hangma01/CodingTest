b5e76235560388c099719a7d398cf25954b466ea
Hangma01 <dn09yooon@gmail.com>
2024-09-12 10:55:55 +0900

    2016년

---
+++ Python/Programmers/Level1/2016년/codingTest.py
@@ -0,0 +1,11 @@
+import datetime
+
+def solution(a, b):
+    week = {0 : 'MON', 1: 'TUE', 2: 'WED', 3: 'THU', 4: 'FRI', 5: 'SAT', 6: 'SUN'}
+    day = datetime.date(2016,a,b)
+
+    return week[day.weekday()]
+    
+a=5
+b=24
+print(solution(a,b))
