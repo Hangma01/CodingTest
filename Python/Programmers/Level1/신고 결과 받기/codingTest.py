2aa868023b3a9aeaf5a3db04218ebd767f4632a7
Hangma01 <dn09yooon@gmail.com>
2024-09-29 19:22:17 +0900

    test

---
+++ Python/Programmers/Level1/신고 결과 받기/codingTest.py
@@ -0,0 +1,15 @@
+def solution(id_list, report, k):
+
+    answer = {i : 0 for i in id_list}
+    
+
+    answer = []
+    return answer
+
+
+
+id_list=["muzi", "frodo", "apeach", "neo"]
+report=["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+k=2
+
+print(solution(id_list,report,k))
